0a88bc92ad0d643e665d33e873eb18f5cbfcd15c
fayezosaadi <fayezosaadi@gmail.com>
2017-04-25 20:35:49 +0000

    Knex add people code

--- add_person.js
+++ add_person.js
@@ -16,9 +16,6 @@ const firstName = process.argv[2];
 const lastName = process.argv[3];
 const dateOfBirth = process.argv[4];
 
-
-
-
 knex('famous_people').insert({first_name: firstName, last_name: lastName, birthdate: dateOfBirth}) .asCallback((err, rows) => {
   if(err){
     console.log(err)
